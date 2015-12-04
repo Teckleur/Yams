d21daf1f428d4dad71674aaf28b5549cc25d699c
Corinne BAUDENS <baudensc@hevea09.iut-infobio.priv.univ-lille1.fr>
2015-12-04 11:18:35 +0100

    Ajout class De

---
+++ test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			De dé1 = new De();
+			System.out.println(dé1.getValeur());
+			
+	}
+
+}
